7a5592a202142a44b771e3411a2c98de006c342c
Daniil Sukharnikov <afterallspace@gmail.com>
2025-07-16 20:52:33 +0300

    app: prepare meta & adaptive

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex text-[5rem] text-center justify-center items-center w-dvw h-dvh bg-green-300 text-shadow-green-700">
+  <div class="text-[5rem] bg-green-300 text-shadow-green-700 max-md:text-[2rem]">
     <h1>Nuxt v4 SSR Performance Test</h1>
   </div>
 </template>
@@ -8,11 +8,13 @@
 useHead({
   title: 'Nuxt V4',
   htmlAttrs: { lang: 'en' },
+  meta: [{ name: 'description', content: 'Just Nuxt v4 + Tailwind v4 Test project' }],
 })
 </script>
 
 <style>
 div {
   font-family: 'Marmelad', sans-serif;
+  @apply flex text-center justify-center items-center w-dvw h-dvh;
 }
 </style>

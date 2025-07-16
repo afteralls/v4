0cae80bb6b676e0918d0085aaadabfcac0590aef
Daniil Sukharnikov <afterallspace@gmail.com>
2025-07-16 21:05:47 +0300

    app: stable

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-3 w-dvw h-dvh bg-green-300 text-shadow-green-700">
+  <div class="flex flex-col gap-3 w-dvw h-dvh bg-green-300 text-green-900">
     <h1 class="text-[5rem] max-md:text-[2rem]">Nuxt v4 SSR Performance Test</h1>
     <span class="text-[2rem] max-md:text-[1rem]">with @nuxt/fonts & Tailwind v4</span>
   </div>

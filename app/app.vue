b9da25c332b6fc82a56ce1d677256712fead467f
Daniil Sukharnikov <afterallspace@gmail.com>
2025-07-16 21:08:17 +0300

    app: release version

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-3 w-dvw h-dvh bg-green-300 text-green-900">
+  <div class="font-[Marmelad] flex text-center justify-center flex-col gap-3 w-dvw h-dvh bg-green-300 text-green-900">
     <h1 class="text-[5rem] max-md:text-[2rem]">Nuxt v4 SSR Performance Test</h1>
     <span class="text-[2rem] max-md:text-[1rem]">with @nuxt/fonts & Tailwind v4</span>
   </div>
@@ -15,10 +15,3 @@ useHead({
   ],
 })
 </script>
-
-<style>
-div {
-  font-family: 'Marmelad', sans-serif;
-  @apply text-center justify-center items-center;
-}
-</style>
